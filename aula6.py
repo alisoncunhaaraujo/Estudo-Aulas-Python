6cbc11b21ca8ff65edaea8f5790a122f4865b61f
Alison <alisonfoxnet@gmail.com>
2022-04-28 11:55:55 -0300

    Atualizando as aulas

---
+++ aula6.py
@@ -0,0 +1,22 @@
+numero_secreto = 42
+total_de_tentativas = 3
+
+
+for rodada in range (1 , total_de_tentativas) + 1:
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite o numero: ")
+    print ("Voce digitou" , chute_str)
+    chute = int(chute_str)
+
+    acertou = chute == numero_secreto
+    maior   = chute >  numero_secreto
+    menor   = chute <  numero_secreto
+
+    if(acertou):
+        print("Voce acertou")
+    else: 
+        if(maior):
+            print("Voce errou, seu chute foi maior que o numero secreto.")
+        elif(menor):
+            print("Voce acertou, seu chute foi menor que o numero secreto.")
+
---
+++ aula7.py
@@ -0,0 +1,27 @@
+numero_secreto = 42
+total_de_tentativas = 3
+
+
+for rodada in range (1 , total_de_tentativas) + 1:
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite um numero entre 1 e 100: ")
+    print ("Voce digitou" , chute_str)
+    chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print("Voce deve digitar um numero entre 1 e 100.")
+        continue
+
+    acertou = chute == numero_secreto
+    maior   = chute >  numero_secreto
+    menor   = chute <  numero_secreto
+
+    if(acertou):
+        print("Voce acertou")
+        break
+    else: 
+        if(maior):
+            print("Voce errou, seu chute foi maior que o numero secreto.")
+        elif(menor):
+            print("Voce acertou, seu chute foi menor que o numero secreto.")
+
