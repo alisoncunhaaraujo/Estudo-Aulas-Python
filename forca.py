658018cfaeb42fabd805f913417380ec856351a6
Your NameAlison <alisonfoxnet@gmail.com>
2022-05-04 11:56:04 -0300

    Atualizando as aulas

---
+++ forca.py
@@ -0,0 +1,10 @@
+# -*- coding: cp1252 -*-
+def jogar():
+    print("********************************")
+    print("Bem vindo ao jogo de Forca")
+    print("********************************")
+
+
+
+
+            
